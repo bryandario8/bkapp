075018f213acb7bfb64a26589063daf86dee80a6
Bryan Tumbaco Moreira <bryandario1994@gmail.com>
2018-08-08 12:49:15 -0500

    Update Signup-test.js

--- __tests__/Signup-test.js
+++ __tests__/Signup-test.js
@@ -6,7 +6,7 @@ import Registros from '../src/pages/Registros'
 import renderer from 'react-test-renderer'
 
 beforeEach(() => {
-  jest.spyOn(console, 'error')
+  jest.spyOn(global.console, 'error')
   global.console.error.mockImplementation(() => {})
 })
 
