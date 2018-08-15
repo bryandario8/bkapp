e3ddc8641472ee2d392ee62f20976f8a5710332e
TaiEspol <taiyuchen92286@gmail.com>
2018-08-15 03:03:47 -0500

    add image

--- src/components/Viewloading.js
+++ src/components/Viewloading.js
@@ -5,17 +5,19 @@ import {
 } from 'native-base'
 import {
   Image,
-  StyleSheet
+  StyleSheet,
+  Dimensions
 } from 'react-native'
-
+var {width} = Dimensions.get('window').width
+var {height} = Dimensions.get('window').height
 // Clase de la pantalla de carga
 export default class Viewloading extends Component {
   // retorna una vista de imagen de carga
   render () {
     return (
-      <Container>
-        <Content style={styles.contenedor}>
-          <Image style={{flex: 1 }} source={require('../images/cargando2.png')} />
+      <Container style={styles.contenedor}>
+        <Content>
+          <Image style={{ flex: 1 }} source={require('../images/cargando2.png')} />
         </Content>
       </Container>
     )
@@ -26,7 +28,7 @@ const styles = StyleSheet.create({
 
   contenedor: {
     flex: 1,
-    width: '100%',
-    height: '100%'
+    width: width,
+    height: height
   }
 })
